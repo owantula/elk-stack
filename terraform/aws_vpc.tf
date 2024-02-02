add2b6a88b385aff2ef12a70218b689e36e3f2e1
Ondřej Wantula <ondrej.wantula@gmail.com>
2024-02-02 09:04:02 +0100

    Add a private and public subnet

--- terraform/aws_vpc.tf
+++ terraform/aws_vpc.tf
@@ -4,3 +4,20 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.0.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "main-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.1.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "main-private-subnet"
+  }
+}

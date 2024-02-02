927393fd8e86da42de7781c789d357464f248f96
Ondřej Wantula <ondrej.wantula@gmail.com>
2024-02-02 07:30:21 +0100

    Add required versions for Terraform and AWS provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+terraform {
+  required_version = "1.7.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.34.0"
+    }
+  }
+}

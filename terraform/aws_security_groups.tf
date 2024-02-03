a86bc0dc5eda5e7ad33c07f1f5f9ab26897cacf4
Ondřej Wantula <ondrej.wantula@gmail.com>
2024-02-03 19:44:11 +0100

    Add temaining rules to security groups so ELK works

--- terraform/aws_security_groups.tf
+++ terraform/aws_security_groups.tf
@@ -1,114 +1,142 @@
 locals {
   ingress_rules = {
-    # puppet-server-ssh-from-internet = {
-    #   description                  = "Allow SSH traffic from internet to the puppet-server"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 22
-    #   to_port                      = 22
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = "0.0.0.0/0"
-    #   referenced_security_group_id = null
-    # }
-
-    # puppet-server-puppet-communication-from-elk-stack = {
-    #   description                  = "Allow puppet traffic to the puppet-server from elk-stack"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 8140
-    #   to_port                      = 8140
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = null
-    #   referenced_security_group_id = aws_security_group.main["elk-stack"].id
-    # }
-    elk-stack-ssh-from-internet = {
-      description                  = "Allow SSH traffic to the elk-stacl from internet"
+    kibana-ssh-from-internet = {
+      description                  = "Allow SSH traffic to kibanal from internet"
       ip_protocol                  = "tcp"
       from_port                    = 22
       to_port                      = 22
-      security_group_id            = aws_security_group.main["elk-stack"].id
+      security_group_id            = aws_security_group.main["kibana"].id
       cidr_ipv4                    = "0.0.0.0/0"
       referenced_security_group_id = null
     }
 
-    # elk-stack-puppet-communication-from-puppet-server = {
-    #   description                  = "Allow puppet traffic to the elk-stack from puppet-server"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 8140
-    #   to_port                      = 8140
-    #   security_group_id            = aws_security_group.main["elk-stack"].id
-    #   cidr_ipv4                    = null
-    #   referenced_security_group_id = aws_security_group.main["puppet-server"].id
-    # }
+    kibana-5601-from-internet = {
+      description                  = "Allow SSH traffic to kibanal from internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 5601
+      to_port                      = 5601
+      security_group_id            = aws_security_group.main["kibana"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
 
+    elasticsearch-ssh-from-kibana = {
+      description                  = "Allow SSH traffic to elasticsearch from internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      security_group_id            = aws_security_group.main["elasticsearch"].id
+      cidr_ipv4                    = null
+      referenced_security_group_id = aws_security_group.main["kibana"].id
+    }
 
+    elasticsearch-9200-from-kibana = {
+      description                  = "Allow SSH traffic to elasticsearch from internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 9200
+      to_port                      = 9200
+      security_group_id            = aws_security_group.main["elasticsearch"].id
+      cidr_ipv4                    = null
+      referenced_security_group_id = aws_security_group.main["kibana"].id
+    }
 
+    elasticsearch-9200-from-logstash = {
+      description                  = "Allow SSH traffic to elasticsearch from internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 9200
+      to_port                      = 9200
+      security_group_id            = aws_security_group.main["elasticsearch"].id
+      cidr_ipv4                    = null
+      referenced_security_group_id = aws_security_group.main["logstash"].id
+    }
+
+    logstash-ssh-from-kibana = {
+      description                  = "Allow SSH traffic to elasticsearch from internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 22
+      to_port                      = 22
+      security_group_id            = aws_security_group.main["logstash"].id
+      cidr_ipv4                    = null
+      referenced_security_group_id = aws_security_group.main["kibana"].id
+    }
   }
   egress_rules = {
-    # puppet-server-to-elk-stack-ssh = {
-    #   description                  = "Allow SSH traffic from the puppet-server to the elk-stack"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 22
-    #   to_port                      = 22
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = null
-    #   referenced_security_group_id = aws_security_group.main["elk-stack"].id
-    # }
-    # puppet-server-to-internet-http = {
-    #   description                  = "Allow http traffic from the puppet-server to the internet"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 80
-    #   to_port                      = 80
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = "0.0.0.0/0"
-    #   referenced_security_group_id = null
-    # }
-    # puppet-server-to-internet-https = {
-    #   description                  = "Allow https traffic from the puppet-server to the internet"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 443
-    #   to_port                      = 443
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = "0.0.0.0/0"
-    #   referenced_security_group_id = null
-    # }
-
-    # puppet-server-puppet-communication-from-elk-stack = {
-    #   description                  = "Allow puppet traffic to the elk-stack from puppet-srver"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 8140
-    #   to_port                      = 8140
-    #   security_group_id            = aws_security_group.main["puppet-server"].id
-    #   cidr_ipv4                    = null
-    #   referenced_security_group_id = aws_security_group.main["elk-stack"].id
-    # }
-
-    elk-stack-to-internet-http = {
-      description                  = "Allow http traffic from the elk-stack to the internet"
+    kibana-to-internet-http = {
+      description                  = "Allow http traffic from kibana to the internet"
       ip_protocol                  = "tcp"
       from_port                    = 80
       to_port                      = 80
-      security_group_id            = aws_security_group.main["elk-stack"].id
+      security_group_id            = aws_security_group.main["kibana"].id
       cidr_ipv4                    = "0.0.0.0/0"
       referenced_security_group_id = null
     }
-    elk-stack-to-internet-https = {
-      description                  = "Allow https traffic from the elk-stack to the internet"
+    kibana-to-internet-https = {
+      description                  = "Allow https traffic from kibana to the internet"
       ip_protocol                  = "tcp"
       from_port                    = 443
       to_port                      = 443
-      security_group_id            = aws_security_group.main["elk-stack"].id
+      security_group_id            = aws_security_group.main["kibana"].id
       cidr_ipv4                    = "0.0.0.0/0"
       referenced_security_group_id = null
     }
 
-    # elk-stack-puppet-communication-from-puppet-server = {
-    #   description                  = "Allow puppet traffic to the puppet-server from elk-stack"
-    #   ip_protocol                  = "tcp"
-    #   from_port                    = 8140
-    #   to_port                      = 8140
-    #   security_group_id            = aws_security_group.main["elk-stack"].id
-    #   cidr_ipv4                    = null
-    #   referenced_security_group_id = aws_security_group.main["puppet-server"].id
-    # }
+    kibana-to-elasticsearch-9200 = {
+      description                  = "Allow traffic from kibana to elasticsearch"
+      ip_protocol                  = "tcp"
+      from_port                    = 9200
+      to_port                      = 9200
+      security_group_id            = aws_security_group.main["kibana"].id
+      cidr_ipv4                    = null
+      referenced_security_group_id = aws_security_group.main["elasticsearch"].id
+    }
+    elasticsearch-to-internet-http = {
+      description                  = "Allow http traffic from kibana to the internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 80
+      to_port                      = 80
+      security_group_id            = aws_security_group.main["elasticsearch"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
+    elasticsearch-to-internet-https = {
+      description                  = "Allow https traffic from kibana to the internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 443
+      to_port                      = 443
+      security_group_id            = aws_security_group.main["elasticsearch"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
+
+    logstash-to-internet-http = {
+      description                  = "Allow http traffic from kibana to the internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 80
+      to_port                      = 80
+      security_group_id            = aws_security_group.main["logstash"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
+    logstash-to-internet-https = {
+      description                  = "Allow https traffic from kibana to the internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 443
+      to_port                      = 443
+      security_group_id            = aws_security_group.main["logstash"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
+
+    logstash-to-elasticsearch-9200 = {
+      description                  = "Allow https traffic from kibana to the internet"
+      ip_protocol                  = "tcp"
+      from_port                    = 9200
+      to_port                      = 9200
+      security_group_id            = aws_security_group.main["logstash"].id
+      cidr_ipv4                    = "0.0.0.0/0"
+      referenced_security_group_id = null
+    }
+
   }
 }
 
